d290657146daf07e69e42a77693cc89b588fae04
Roman Tymchyshyn <romantymchyshyn.rt@gmail.com>
2015-09-05 14:07:37 +0300

    dialog box for bad user input

---
+++ Spider/DialogBox.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Spider
+{
+    partial class DialogBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Location = new System.Drawing.Point(42, 38);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(214, 140);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(96, 214);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(97, 25);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // DialogBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.messageLabel);
+            this.Name = "DialogBox";
+            this.Text = "DialogBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ Spider/DialogBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spider
+{
+    public partial class DialogBox : Form
+    {
+        public DialogBox(string message)
+        {
+            InitializeComponent();
+            messageLabel.Text = message;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
